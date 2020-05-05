9d1fb6305cd143dee8dcc932fd43a2cffa5cf3dd
RasterDX <rasterplane@hotmail.com>
2020-05-05 02:53:56 -0700

    Commit

---
+++ Data/Asp_Maftei_Razvan_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Maftei_Razvan_Rp.Models;
+
+namespace Asp_Maftei_Razvan_Rp.Data
+{
+    public class Asp_Maftei_Razvan_RpContext : DbContext
+    {
+        public Asp_Maftei_Razvan_RpContext (DbContextOptions<Asp_Maftei_Razvan_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Maftei_Razvan_Rp.Models.Movie> Movie { get; set; }
+    }
+}
